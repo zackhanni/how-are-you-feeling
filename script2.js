757190e28b259e8625f81ca2021a17266461b693
Zack Hanni <zack.hannI@gmail.com>
2023-07-24 10:31:06 -0400

    targeting html elements

--- script2.js
+++ script2.js
@@ -1,28 +1,41 @@
-const questionElement = document.getElementById("question")
-const feelingButtonsElement = document.getElementById("feeling-buttons")
+const questionElement = document.querySelector("#question")
+const feelingButtonsElement = document.querySelector("#feeling-buttons")
 
 
 
 
 function beginFeelingsJournal() {
     
+    targetQuestion(currentNode())
+    targetFeelingButtons(currentNode())
+    // alert("begin feelings journal has been run")
 }
 
 // find out and keep track of current feelingNodes.id should be displayed on screen
-function whichFeelingNode() {
-    let currentFeelingNode = 1
-    return currentFeelingNode
+function currentNode() {
+    let node = feelingNodes[0].id - 1
+    return node
 }
 
 // target id 'question' and replace innervalue with the value of feelingNodes.question
-function targetQuestion(questionArrayValue) {
-    const z = questionElement
-    z.innerHTML = feelingNodes[currentFeelingNode]
+function targetQuestion(nodeId) {
+    const question = questionElement
+    question.innerHTML = ""
+    question.innerHTML = feelingNodes[nodeId].question
 }
 
 //create a <button> for each value stored in feelingNodes.options.text depending on current feelingNodes.id value
-function targetFeelingButtons() {
+function targetFeelingButtons(z) {
+    let button = feelingButtonsElement.children[0].innerHTML
+    button = feelingNodes[0].options[0].text
 
+
+    alert(button)
+
+   // document.querySelector("#feeling-buttons > button:nth-child(1)")
+
+    
+    // document.createElement("button").classList.add("feeling-btn")
 }
 
 
@@ -30,3 +43,4 @@ function targetFeelingButtons() {
 
 
 beginFeelingsJournal();
+// alert("this is the end of the script2 page")
