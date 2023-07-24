0e8cad49c020f6e21d315f4f9eca5d4100dc81a8
Zack Hanni <zack.hannI@gmail.com>
2023-07-24 17:08:38 -0400

    add more feeling options

--- feelingNodes.js
+++ feelingNodes.js
@@ -11,6 +11,10 @@ const feelingNodes = [
         {
           text: "Sad",
           nextFeeling: 3
+        },
+        {
+          text: "Angry",
+          nextFeeling: 4
         }
       ]
     },
@@ -24,17 +28,45 @@ const feelingNodes = [
         },
         {
           text: "Playful",
-          nextFeeling: 2
+          nextFeeling: 20
         },
         {
           text: "Content",
-          nextFeeling: 2
+          nextFeeling: 20
+        },
+        {
+          text: "Interested",
+          nextFeeling: 20
+        },
+        {
+          text: "Proud",
+          nextFeeling: 20
+        },
+        {
+          text: "Accepted",
+          nextFeeling: 20
+        },
+        {
+          text: "Powerful",
+          nextFeeling: 20
+        },
+        {
+          text: "Peaceful",
+          nextFeeling: 20
+        },
+        {
+          text: "Trusting",
+          nextFeeling: 20
+        },
+        {
+          text: "Optimistict",
+          nextFeeling: 20
         }
       ]
     },
     {
         id: 3,
-        question: "Lets find a more specific description.",
+        question: "Sad. Lets find a more specific description.",
         options: [
           {
             text: 'Nevermind, go back.',
@@ -42,12 +74,115 @@ const feelingNodes = [
           },
           {
             text: "Lonely",
-            nextFeeling: 3
+            nextFeeling: 30
           },
           {
             text: "Vulnerable",
-            nextFeeling: 3
+            nextFeeling: 30
+          },
+          {
+            text: "Despair",
+            nextFeeling: 30
+          },
+          {
+            text: "Guilty",
+            nextFeeling: 30
+          },
+          {
+            text: "Depressed",
+            nextFeeling: 30
+          },
+          {
+            text: "Hurt",
+            nextFeeling: 30
           }
         ]
       },
+      {
+          id: 4,
+          question: "Angry. Lets find a more specific description.",
+          options: [
+            {
+              text: 'Nevermind, go back.',
+              nextFeeling: 1
+            },
+            {
+              text: "Let down",
+              nextFeeling: 40
+            },
+            {
+              text: "Humiliated",
+              nextFeeling: 40
+            },
+            {
+              text: "Bitter",
+              nextFeeling: 40
+            },
+            {
+              text: "Mad",
+              nextFeeling: 40
+            },
+            {
+              text: "Aggressive",
+              nextFeeling: 40
+            },
+            {
+              text: "Frusterated",
+              nextFeeling: 40
+            },
+            {
+              text: "Distant",
+              nextFeeling: 40
+            },
+            {
+              text: "Critical",
+              nextFeeling: 40
+            }
+          ]
+        },
+        {
+            id: 20,
+            question: "Happy. Once I can develop this app further, there will be more information here!",
+            options: [
+              {
+                text: 'Nevermind, go back.',
+                nextFeeling: 2
+              },
+              {
+                text: "Restart",
+                nextFeeling: 1
+              }
+
+            ]
+          },
+          {
+              id: 30,
+              question: "Sad. Once I can develop this app further, there will be more information here!",
+              options: [
+                {
+                  text: 'Nevermind, go back.',
+                  nextFeeling: 3
+                },
+                {
+                  text: "Restart",
+                  nextFeeling: 1
+                }
+  
+              ]
+            },
+            {
+                id: 40,
+                question: "Angry. Once I can develop this app further, there will be more information here!",
+                options: [
+                  {
+                    text: 'Nevermind, go back.',
+                    nextFeeling: 4
+                  },
+                  {
+                    text: "Restart",
+                    nextFeeling: 1
+                  }
+    
+                ]
+              }
   ]
