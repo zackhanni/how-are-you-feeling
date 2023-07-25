97a888e7fa4c1563ebd72d77ac03265184b7decc
Zack Hanni <zack.hannI@gmail.com>
2023-07-25 02:13:17 -0400

    changed spacing

--- feelingNodes.js
+++ feelingNodes.js
@@ -65,124 +65,124 @@ const feelingNodes = [
       ]
     },
     {
-        id: 3,
-        question: "You feel sad. Remember that it is not perminant. Let's find a more specific description.",
-        options: [
-          {
-            text: 'Go back',
-            nextFeeling: 1
-          },
-          {
-            text: "Lonely",
-            nextFeeling: 30
-          },
-          {
-            text: "Vulnerable",
-            nextFeeling: 30
-          },
-          {
-            text: "Despair",
-            nextFeeling: 30
-          },
-          {
-            text: "Guilty",
-            nextFeeling: 30
-          },
-          {
-            text: "Depressed",
-            nextFeeling: 30
-          },
-          {
-            text: "Hurt",
-            nextFeeling: 30
-          }
-        ]
-      },
-      {
-          id: 4,
-          question: "You feel angry. Take a deep breath. Let's find a more specific description.",
-          options: [
-            {
-              text: 'Go back',
-              nextFeeling: 1
-            },
-            {
-              text: "Let down",
-              nextFeeling: 40
-            },
-            {
-              text: "Humiliated",
-              nextFeeling: 40
-            },
-            {
-              text: "Bitter",
-              nextFeeling: 40
-            },
-            {
-              text: "Mad",
-              nextFeeling: 40
-            },
-            {
-              text: "Aggressive",
-              nextFeeling: 40
-            },
-            {
-              text: "Frusterated",
-              nextFeeling: 40
-            },
-            {
-              text: "Distant",
-              nextFeeling: 40
-            },
-            {
-              text: "Critical",
-              nextFeeling: 40
-            }
-          ]
+      id: 3,
+      question: "You feel sad. Remember that it is not perminant. Let's find a more specific description.",
+      options: [
+        {
+          text: 'Go back',
+          nextFeeling: 1
         },
         {
-            id: 20,
-            question: "Happy. Once I can develop this app further, there will be more information here!",
-            options: [
-              {
-                text: 'Go back',
-                nextFeeling: 2
-              },
-              {
-                text: "Restart",
-                nextFeeling: 1
-              }
+          text: "Lonely",
+          nextFeeling: 30
+        },
+        {
+          text: "Vulnerable",
+          nextFeeling: 30
+        },
+        {
+          text: "Despair",
+          nextFeeling: 30
+        },
+        {
+          text: "Guilty",
+          nextFeeling: 30
+        },
+        {
+          text: "Depressed",
+          nextFeeling: 30
+        },
+        {
+          text: "Hurt",
+          nextFeeling: 30
+        }
+      ]
+    },
+    {
+      id: 4,
+      question: "You feel angry. Take a deep breath. Let's find a more specific description.",
+      options: [
+        {
+          text: 'Go back',
+          nextFeeling: 1
+        },
+        {
+          text: "Let down",
+          nextFeeling: 40
+        },
+        {
+          text: "Humiliated",
+          nextFeeling: 40
+        },
+        {
+          text: "Bitter",
+          nextFeeling: 40
+        },
+        {
+          text: "Mad",
+          nextFeeling: 40
+        },
+        {
+          text: "Aggressive",
+          nextFeeling: 40
+        },
+        {
+          text: "Frusterated",
+          nextFeeling: 40
+        },
+        {
+          text: "Distant",
+          nextFeeling: 40
+        },
+        {
+          text: "Critical",
+          nextFeeling: 40
+        }
+      ]
+    },
+    {
+      id: 20,
+      question: "Happy. Once I can develop this app further, there will be more information here!",
+      options: [
+        {
+          text: 'Go back',
+          nextFeeling: 2
+        },
+        {
+          text: "Restart",
+          nextFeeling: 1
+        }
 
-            ]
-          },
-          {
-              id: 30,
-              question: "Sad. Once I can develop this app further, there will be more information here!",
-              options: [
-                {
-                  text: 'Go back',
-                  nextFeeling: 3
-                },
-                {
-                  text: "Restart",
-                  nextFeeling: 1
-                }
-  
-              ]
-            },
-            {
-                id: 40,
-                question: "Angry. Once I can develop this app further, there will be more information here!",
-                options: [
-                  {
-                    text: 'Go back',
-                    nextFeeling: 4
-                  },
-                  {
-                    text: "Restart",
-                    nextFeeling: 1
-                  }
-    
-                ]
-              }
+      ]
+    },
+    {
+      id: 30,
+      question: "Sad. Once I can develop this app further, there will be more information here!",
+      options: [
+        {
+          text: 'Go back',
+          nextFeeling: 3
+        },
+        {
+          text: "Restart",
+          nextFeeling: 1
+        }
+
+      ]
+    },
+    {
+      id: 40,
+      question: "Angry. Once I can develop this app further, there will be more information here!",
+      options: [
+        {
+          text: 'Go back',
+          nextFeeling: 4
+        },
+        {
+          text: "Restart",
+          nextFeeling: 1
+        }
+
+      ]
+    }
   ]
