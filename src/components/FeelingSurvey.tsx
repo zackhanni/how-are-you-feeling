ec1df260dfafa0514fdb5e18a3fbd9e5150d60a8
Zack Hanni <zack.hannI@gmail.com>
2023-07-26 16:20:05 -0400

    search FeelingNodes for a specific node and update content accordingly

--- src/components/FeelingSurvey.tsx
+++ src/components/FeelingSurvey.tsx
@@ -2,20 +2,27 @@ import { FeelingButton } from "./FeelingButton";
 import { FeelingQuestion } from "@/components/FeelingQuestion"
 
 import FeelingNodes from "./FeelingNodes";
-let nodeId = 0;
-let feelingOptions = FeelingNodes[nodeId].options
-let question = FeelingNodes[nodeId].question
+
+let nodeId:Number = 1;
+const result = FeelingNodes.find(({ id }) => id === nodeId );
 
 export const FeelingSurvey = () => { 
+
+    function changeNode(nodeInput:Number) {
+        nodeInput = nodeId
+    }
+
     return (
         <div className="container">
-            <FeelingQuestion question={question}/> 
-        <div id="feeling-buttons" className="btn-grid">
-            <FeelingButton feeling={feelingOptions[0].text} />
-            <FeelingButton feeling={feelingOptions[1].text}/>
-            <FeelingButton feeling={feelingOptions[2].text}/>
-            <FeelingButton feeling="other" />
-        </div>
+            <FeelingQuestion question={result.question}/> 
+            <div id="feeling-buttons" className="btn-grid">
+                
+                <FeelingButton feeling={result.options[0].text} onClick={changeNode(2)} />
+                <FeelingButton feeling={result.options[1].text} onClick={changeNode(3)} />
+                <FeelingButton feeling={result.options[2].text} onClick={changeNode(4)} />
+                <FeelingButton feeling="other" />
+                <h1>result</h1>
+            </div>
         </div>
     )
 };
