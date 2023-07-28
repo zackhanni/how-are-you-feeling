83c304f9b9200eb38363498f144173aec6d2ed30
Zack Hanni <zack.hannI@gmail.com>
2023-07-28 14:39:18 -0400

    keeping as a backup/ for reference

--- src/components/FeelingSurvey.tsx
+++ src/components/FeelingSurvey.tsx
@@ -1,13 +1,14 @@
-import FeelingNodes from "./FeelingNodes";
+import FeelingData from "./FeelingData";
 
 const FeelingSurvey = (props) => { 
 
-    const feelingNode = FeelingNodes.find(({ id }) => id === props.id );
+    const feelingNode = FeelingData.find(({ id }) => id === props.id );
 
     return (
         <div className="container">
             <div id="question">{feelingNode.question}</div>
             <div id="feeling-buttons" className="btn-grid">
+                
                 {feelingNode?.options.map(option => 
                     // let idValue = {option.nextFeeling};
                     <button className="feeling-btn" id={option.nextFeeling} >
