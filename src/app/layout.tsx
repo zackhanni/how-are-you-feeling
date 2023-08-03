c6e071ef4d377ff299d4fca9c923b4c0d21b1e35
Zack Hanni <zack.hannI@gmail.com>
2023-08-03 19:10:42 -0400

    begin integrating google authentication
    app is still functional at this point

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,21 @@
 import './globals.css'
 import type { Metadata } from 'next'
-//import { Inter } from 'next/font/google'
 
-//const inter = Inter({ subsets: ['latin'] })
+import { SessionProvider } from "next-auth/react"
 
 export const metadata: Metadata = {
   title: 'Emotional Intelligence',
   description: 'Lean about your emotions and how to better express your feelings.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ 
+  
+  children, }: { children: React.ReactNode }) 
+  
+  {
   return (
     <html lang="en">
-      {/* <body className={inter.className}> */}
+
       <body>  
         {children}
       </body>
